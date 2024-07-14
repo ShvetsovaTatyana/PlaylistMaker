6584ecdf0ae3171579d60b15f6b7d79f92347018
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2024-07-14 21:01:01 +0300

    спринт 10. ДЗ

---
+++ app/src/main/java/com/github/ilyashvetsov/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,30 @@
+package com.github.ilyashvetsov.playlistmaker
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+
+class TrackViewHolder(parentView: View) : RecyclerView.ViewHolder(parentView) {
+    private val pictureImageView: ImageView
+    private val nameSingTextView: TextView
+    private val singerTextView: TextView
+    private val timeSingTextView: TextView
+
+    init {
+        pictureImageView = parentView.findViewById(R.id.picture_imageView)
+        nameSingTextView = parentView.findViewById(R.id.name_sing)
+        singerTextView = parentView.findViewById(R.id.name_singer)
+        timeSingTextView = parentView.findViewById(R.id.time_sing)
+    }
+
+    fun bind(model: Track) {
+        Glide.with(pictureImageView).load(model.artworkUrl100)
+            .placeholder(R.drawable.placeholder).into(pictureImageView)
+        nameSingTextView.text = model.trackName
+        singerTextView.text = model.artistName
+        timeSingTextView.text = model.trackTime
+    }
+}
